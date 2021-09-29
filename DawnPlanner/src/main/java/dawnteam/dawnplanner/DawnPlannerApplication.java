cc6f23b338975b68905e02f1c7b228abe6ca0222
문태호 <muntaeho@muntaehoui-iMac-Pro.local>
2021-09-30 00:49:10 +0900

    initial commit

---
+++ DawnPlanner/src/main/java/dawnteam/dawnplanner/DawnPlannerApplication.java
@@ -0,0 +1,13 @@
+package dawnteam.dawnplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawnPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DawnPlannerApplication.class, args);
+    }
+
+}
---
+++ DawnPlanner/src/test/java/dawnteam/dawnplanner/DawnPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package dawnteam.dawnplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawnPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
