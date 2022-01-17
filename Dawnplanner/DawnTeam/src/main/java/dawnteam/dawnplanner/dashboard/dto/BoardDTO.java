7796361e7de3468f916323cdb1ebdd6f24e132eb
moter <moter2588@gmail.com>
2022-01-17 22:58:34 +0900

    [ADD] BoardDTO

--- Dawnplanner/DawnTeam/src/main/java/dawnteam/dawnplanner/dashboard/dto/BoardDTO.java
+++ Dawnplanner/DawnTeam/src/main/java/dawnteam/dawnplanner/dashboard/dto/BoardDTO.java
@@ -1,4 +1,21 @@
 package dawnteam.dawnplanner.dashboard.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Data
 public class BoardDTO {
+    private String boardTitle;
+    private String boardAlias;
+    private String boardDescription;
+    private String author;
+    private Date boardCreated;
+    private Date boardExpired;
 }
